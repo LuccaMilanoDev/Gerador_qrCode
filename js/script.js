4684a36af0d719d381f30514f87b8b5c5ad7e39e
LuccaMilanoDev <luccamilano2018@gmail.com>
2024-06-02 11:35:59 -0300

    finish

--- js/script.js
+++ js/script.js
@@ -10,8 +10,7 @@ function generateQrCode() {
 
   qrCodeBtn.innerText = "Gerando código...";
 
-  qrCodeImg.src =
-    "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrCodeInputValue}";
+  qrCodeImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrCodeInputValue}`;
 
   qrCodeImg.addEventListener("load", () => {
     container.classList.add("active");
